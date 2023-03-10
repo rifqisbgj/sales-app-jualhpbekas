eb27dd9193aa37b931526766b856c82c4b3d950e
Rifqi Subagja <rifqisubagjaa@gmail.com>
2023-03-10 09:49:40 +0700

    update: hide role super and res email exist

--- routes/handler/users/createUser.js
+++ routes/handler/users/createUser.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcrypt");
 const { Users } = require("../../../models");
 const { v1: uuidv1 } = require("uuid");
 const Validator = require("fastest-validator");
+// kustom validasi jika terdapat error
 const valid = new Validator({
   messages: {
     string: "Silahkan cek kembali bidang {field} ",
@@ -11,10 +12,6 @@ const valid = new Validator({
     stringMin: "{field} harus berisi minimal {expected} digit",
     email: "{field} harus berisi format email yang benar",
     enumValue: "{field} harus berisi nilai {expected}",
-    // error handling uuid type
-    uuid: "Kesalahan format pada '{field}'",
-    // error handling uuidVersion validation
-    uuidVersion: "Kesalahan versi UUID",
   },
 });
 
@@ -24,9 +21,10 @@ module.exports = async (req, res) => {
     email: "email|empty:false",
     password: "string|min:6",
     role: { type: "enum", values: ["super", "adminQC", "adminSale"] },
-    created_by: "uuid|version:1",
+    created_by: "email|empty:false",
   };
 
+  // cek inputan dengan schema pengecekan yang telah diberikan
   const validate = valid.validate(req.body, schema);
 
   // if validate have an array it's mean error
@@ -36,33 +34,45 @@ module.exports = async (req, res) => {
       message: validate,
     });
   }
-
   // find email exists
   const user = await Users.findOne({
     where: { email: req.body.email },
   });
 
+  // // get uuid createdBy
+  const createdBy = await Users.findOne({
+    where: { email: req.body.created_by },
+  });
+
+  // jika user sudah tersedia
   if (user) {
-    return res.status(409).json({
-      status: "error",
-      message: "email already exists",
-    });
+    return res.status(409).json([
+      {
+        status: "error",
+        message: "email already exists",
+      },
+    ]);
   }
 
+  // bcrypt password dengan data password yang diinputkan user
   const password = await bcrypt.hash(req.body.password, 10);
 
+  // masukan data yg telah diinputkan user ke container data
   const data = {
+    // generate uuid v1 dan inputkan sebagai data kolom id
     id: uuidv1(),
     password,
     nama: req.body.name,
     email: req.body.email,
     avatar: req.body.avatar,
     role: req.body.role,
-    created_by: req.body.created_by,
+    created_by: createdBy.id,
   };
 
+  // create user
   const createUser = await Users.create(data);
 
+  // res if success
   return res.json({
     status: "success",
     data: createUser,
--- routes/handler/users/getListUser.js
+++ routes/handler/users/getListUser.js
@@ -4,15 +4,25 @@ const { Users } = require("../../../models");
 module.exports = async (req, res) => {
   // mengambil daftar pengguna/admin berdasarkan query
   const namaAdmin = req.query.name_user || [];
+
+  // ambil data dengan antribut yang telah ditentukan
   const sqlOptions = {
     attributes: ["id", "nama", "email", "role", "avatar"],
   };
 
+  // jika terdapat nilai searching user
   if (namaAdmin.length) {
     sqlOptions.where = {
       [Op.or]: [{ nama: { [Op.like]: "%" + namaAdmin + "%" } }],
+      role: { [Op.not]: "super" },
     };
   }
+
+  // ambil user tanpa role super
+  sqlOptions.where = {
+    role: { [Op.not]: "super" },
+  };
+
   // mengambil seluruh data user sesuai dengan sqlOptions
   const users = await Users.findAll(sqlOptions);
 
