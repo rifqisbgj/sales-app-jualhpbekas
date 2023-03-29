5592ef80b093d9f60043d2b15d854420944b4552
Rifqi Subagja <rifqisubagjaa@gmail.com>
2023-03-29 08:30:09 +0700

    add: pagination, search & filter user by role

--- routes/handler/users/getListUser.js
+++ routes/handler/users/getListUser.js
@@ -2,26 +2,33 @@ const { Op } = require("sequelize");
 const { Users } = require("../../../models");
 
 module.exports = async (req, res) => {
-  // mengambil daftar pengguna/admin berdasarkan query
-  const namaAdmin = req.query.name_user || [];
+  const page = parseInt(req.query.page) || 0;
+  const limit = parseInt(req.query.limit) || 10;
+  const search = req.query.search_query || "";
+  const offset = limit * page;
+  const roleAdmin = req.query.role || "";
 
-  // ambil data dengan antribut yang telah ditentukan
-  const sqlOptions = {
-    attributes: ["id", "nama", "email", "role", "avatar", "createdAt"],
-  };
-
-  // jika terdapat nilai searching user
-  if (namaAdmin.length) {
-    sqlOptions.where = {
-      [Op.or]: [{ nama: { [Op.like]: "%" + namaAdmin + "%" } }],
-      role: { [Op.not]: "super" },
-    };
+  const sqlFilter = {};
+  if (roleAdmin !== "") {
+    sqlFilter.role = roleAdmin;
   }
 
-  // ambil user tanpa role super
-  sqlOptions.where = {
-    role: { [Op.not]: "super" },
+  const sqlOptions = {
+    where: {
+      [Op.or]: [
+        { nama: { [Op.iLike]: "%" + search + "%" } },
+        { email: { [Op.iLike]: "%" + search + "%" } },
+      ],
+      [Op.and]: [sqlFilter],
+      [Op.not]: [{ id: req.user.data.id }],
+    },
   };
+
+  const totalRows = await Users.count(sqlOptions);
+  const totalPage = Math.ceil(totalRows / limit);
+
+  sqlOptions.offset = offset;
+  sqlOptions.limit = limit;
   sqlOptions.order = [["createdAt", "DESC"]];
 
   // mengambil seluruh data user sesuai dengan sqlOptions
@@ -30,5 +37,9 @@ module.exports = async (req, res) => {
   return res.json({
     status: "success",
     data: users,
+    page: page,
+    limit: limit,
+    totalRows: totalRows,
+    totalPage: totalPage,
   });
 };
